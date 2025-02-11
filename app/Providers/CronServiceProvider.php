07499d2e17e30d55ea4db0755866701fefb6693b
Arthur Parienté <41431456+arthurpar06@users.noreply.github.com>
2025-02-11 23:34:06 +0100

    Register UpdateSimBriefData Listener (#1999)

--- app/Providers/CronServiceProvider.php
+++ app/Providers/CronServiceProvider.php
@@ -13,6 +13,7 @@ use App\Cron\Nightly\PilotLeave;
 use App\Cron\Nightly\RecalculateBalances;
 use App\Cron\Nightly\RecalculateStats;
 use App\Cron\Nightly\SetActiveFlights;
+use App\Cron\Weekly\UpdateSimbriefData;
 use App\Events\CronFifteenMinute;
 use App\Events\CronFiveMinute;
 use App\Events\CronHourly;
@@ -47,6 +48,7 @@ class CronServiceProvider extends ServiceProvider
             NewVersionCheck::class,
         ],
         CronWeekly::class => [
+            UpdateSimbriefData::class
         ],
         CronMonthly::class => [
             \App\Cron\Monthly\ApplyExpenses::class,
