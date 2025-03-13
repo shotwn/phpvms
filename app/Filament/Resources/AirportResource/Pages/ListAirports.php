97524be8c29b38bf3c01899d976e96491579af0a
RazorBladeX <192748191+RazorBladeX@users.noreply.github.com>
2025-03-13 15:24:27 +0000

    [8.x] Add Import/Export CSV button back to Airports admin page (#2004)
    * adds back csv import/export
    
    * updates airport to airports

--- app/Filament/Resources/AirportResource/Pages/ListAirports.php
+++ app/Filament/Resources/AirportResource/Pages/ListAirports.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\AirportResource\Pages;
 
-use App\Filament\Resources\AirportResource;
 use Filament\Actions;
+use App\Filament\Resources\AirportResource;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Actions\ExportAction;
+use App\Filament\Actions\ImportAction;
+use App\Models\Enums\ImportExportType;
 
 class ListAirports extends ListRecords
 {
@@ -13,6 +16,8 @@ class ListAirports extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            ExportAction::make('export')->arguments(['resourceTitle' => 'airports', 'exportType' => ImportExportType::AIRPORT]),
+            ImportAction::make('import')->arguments(['resourceTitle' => 'airports', 'importType' => ImportExportType::AIRPORT]),
             Actions\CreateAction::make()->label('Add Airport')->icon('heroicon-o-plus-circle'),
         ];
     }
